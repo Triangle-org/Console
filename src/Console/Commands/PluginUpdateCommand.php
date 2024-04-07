0dfa61b738dfeb7d57826c7c792b5c60cc594a7f
Ivan Zorin <creator@localzet.com>
2024-04-07 21:32:27 +0300

    Create PluginUpdateCommand.php

---
+++ src/Console/Commands/PluginUpdateCommand.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @package     Triangle Console Plugin
+ * @link        https://github.com/Triangle-org/Console
+ *
+ * @author      Ivan Zorin <creator@localzet.com>
+ * @copyright   Copyright (c) 2018-2024 Localzet Group
+ * @license     GNU Affero General Public License, version 3
+ *
+ *              This program is free software: you can redistribute it and/or modify
+ *              it under the terms of the GNU Affero General Public License as
+ *              published by the Free Software Foundation, either version 3 of the
+ *              License, or (at your option) any later version.
+ *
+ *              This program is distributed in the hope that it will be useful,
+ *              but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *              MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *              GNU Affero General Public License for more details.
+ *
+ *              You should have received a copy of the GNU Affero General Public License
+ *              along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace Triangle\Console\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Triangle\Console\Util;
+
+/**
+ * @author walkor <walkor@workerman.net>
+ * @author Ivan Zorin <ivan@zorin.space>
+ */
+class PluginUpdateCommand extends Command
+{
+    protected static $defaultName = 'plugin:update';
+    protected static $defaultDescription = 'Обновить плагин';
+
+    /**
+     * @return void
+     */
+    protected function configure(): void
+    {
+        $this->addArgument('name', InputArgument::REQUIRED, 'Название плагина (например, triangle/plugin)');
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $name = $input->getArgument('name');
+        $output->writeln("Обновление плагина $name");
+        $namespace = Util::nameToNamespace($name);
+        $install_function = "\\$namespace\\Install::update";
+        $plugin_const = "\\$namespace\\Install::TRIANGLE_PLUGIN";
+        if (defined($plugin_const) && is_callable($install_function)) {
+            $install_function();
+        }
+        return self::SUCCESS;
+    }
+}
