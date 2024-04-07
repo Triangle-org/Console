fddb2cb27c7c1ec319c7b5690786e0943f9d07cc
Ivan Zorin <creator@localzet.com>
2024-04-07 21:04:28 +0300

    Update Console.php

--- src/Console.php
+++ src/Console.php
@@ -34,6 +34,9 @@ use SplFileInfo;
 use support\Container;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Command\Command as Commands;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputOption;
 
 /**
  *
@@ -81,14 +84,27 @@ class Console extends Application
                 continue;
             }
             $properties = $reflection->getStaticProperties();
-            $name = $properties['defaultName'];
+            $name = $properties['defaultName'] ?? null;
             if (!$name) {
-                throw new RuntimeException("Command $class_name has no defaultName");
+                throw new RuntimeException("У команды $class_name нет defaultName");
             }
             $description = $properties['defaultDescription'] ?? '';
             $command = Container::get($class_name);
             $command->setName($name)->setDescription($description);
             $this->add($command);
         }
+    }
+
+    protected function getDefaultInputDefinition(): InputDefinition
+    {
+        return new InputDefinition([
+            new InputArgument('command', InputArgument::REQUIRED, 'Команда для выполнения'),
+            new InputOption('--help', '-h', InputOption::VALUE_NONE, 'Отобразить справку по данной команде. Если команда не задана, отобразится справка для команды <info>list</info>.'),
+            new InputOption('--quiet', '-q', InputOption::VALUE_NONE, 'Не выводить никаких сообщений'),
+            new InputOption('--verbose', '-v|vv|vvv', InputOption::VALUE_NONE, 'Увеличьте уровень детализации сообщений: 1 - для обычного вывода, 2 - для более подробного вывода и 3 - для отладки.'),
+            new InputOption('--version', '-V', InputOption::VALUE_NONE, 'Показать версию приложения'),
+            new InputOption('--ansi', '', InputOption::VALUE_NEGATABLE, 'Принудительно вывести ANSI (или --no-ansi чтобы отключить ANSI)', null),
+            new InputOption('--no-interaction', '-n', InputOption::VALUE_NONE, 'Не задавайте интерактивных вопросов'),
+        ]);
     }
 }
