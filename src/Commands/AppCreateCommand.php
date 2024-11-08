34cb259b1b1df455d75ee79a771a0af7ee84e75e
Ivan Zorin <creator@localzet.com>
2024-11-08 23:04:18 +0300

    Update AppCreateCommand.php

--- src/Commands/AppCreateCommand.php
+++ src/Commands/AppCreateCommand.php
@@ -134,7 +134,7 @@ class Index
     {
         return response('Добро пожаловать в $name!');
     }
-
+    
 }
 
 EOF;
@@ -153,10 +153,9 @@ EOF;
         $content = <<<EOF
 <?php
 
-use support\\Request;
-
 return [
     'debug' => true,
+    'name' => '$name',
     
     'controller_suffix' => env('CONTROLLER_SUFFIX', ''),
     'controller_reuse' => env('CONTROLLER_REUSE', true),
@@ -247,45 +246,45 @@ EOF;
         file_put_contents("$base/middleware.php", $content);
 
         // process.php
-        $content = <<<EOF
-<?php
-
-return [];
-
-EOF;
-        file_put_contents("$base/process.php", $content);
+//        $content = <<<EOF
+//<?php
+//
+//return [];
+//
+//EOF;
+//        file_put_contents("$base/process.php", $content);
 
         // redis.php
-        $content = <<<EOF
-<?php
-
-return [
-    'client' => env('REDIS_CLIENT', 'predis'),
-
-    'options' => [
-        'cluster' => env('REDIS_CLUSTER', 'redis'),
-        'prefix' => env('REDIS_PREFIX', 'triangle_'),
-    ],
-
-    'default' => [
-        'url' => env('REDIS_URL'),
-        'host' => env('REDIS_HOST', '127.0.0.1'),
-        'port' => env('REDIS_PORT', '6379'),
-        'password' => env('REDIS_PASSWORD'),
-        'database' => env('REDIS_DB', '0'),
-    ],
-
-    'cache' => [
-        'url' => env('REDIS_URL'),
-        'host' => env('REDIS_HOST', '127.0.0.1'),
-        'port' => env('REDIS_PORT', '6379'),
-        'password' => env('REDIS_PASSWORD'),
-        'database' => env('REDIS_DB_CACHE', '1'),
-    ],
-];
-
-EOF;
-        file_put_contents("$base/redis.php", $content);
+//        $content = <<<EOF
+//<?php
+//
+//return [
+//    'client' => env('REDIS_CLIENT', 'predis'),
+//
+//    'options' => [
+//        'cluster' => env('REDIS_CLUSTER', 'redis'),
+//        'prefix' => env('REDIS_PREFIX', 'triangle_'),
+//    ],
+//
+//    'default' => [
+//        'url' => env('REDIS_URL'),
+//        'host' => env('REDIS_HOST', '127.0.0.1'),
+//        'port' => env('REDIS_PORT', '6379'),
+//        'password' => env('REDIS_PASSWORD'),
+//        'database' => env('REDIS_DB', '0'),
+//    ],
+//
+//    'cache' => [
+//        'url' => env('REDIS_URL'),
+//        'host' => env('REDIS_HOST', '127.0.0.1'),
+//        'port' => env('REDIS_PORT', '6379'),
+//        'password' => env('REDIS_PASSWORD'),
+//        'database' => env('REDIS_DB_CACHE', '1'),
+//    ],
+//];
+//
+//EOF;
+//        file_put_contents("$base/redis.php", $content);
 
         // route.php
         $content = <<<EOF
